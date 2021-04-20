a2def95f345a3fbf8a200dbc548319d160fc5dd8
임서영 <sylim0318@naver.com>
2021-04-20 15:19:19 +0900

    매개변수를 모르는 경우

---
+++ 매개변수/CARRRR.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class CARRRR {
+    int sum(int[] v1){
+        int sum = 0;
+        for(int i = 0; i < v1.length; i++){
+            sum += v1[i];
+        }
+        return sum;
+    }
+
+
+
+}
---
+++ 매개변수/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        CARRRR C1 = new CARRRR();
+
+        int[] v1 =  {1,2,3};
+
+        int result = C1.sum(v1);
+
+        System.out.println("답: " +result);
+    }
+}
